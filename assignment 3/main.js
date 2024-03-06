9f4a37ef038303306d72058ad99528e586798e99
sehrish <sehrishiftikhar720@gmail.com>
2024-03-06 10:29:42 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Nothing is impossible because impossible itself I M POSSIBLE";
+let author = "sehrish Iftikhar";
+//printing the massage
+console.log(`"${quote}" - ${author}`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impossible because impossible itself I M POSSIBLE";
+
+let author:string = "sehrish Iftikhar";
+
+//printing the massage
+
+console.log(`"${quote}" - ${author}` );
